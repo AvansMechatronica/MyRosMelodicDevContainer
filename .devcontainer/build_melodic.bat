dd8ba5fed982d9e979cac68ebecd5c635d1d5f9c
Gerard Harkema <ga.harkema@avans.nl>
2024-06-25 14:10:53 +0200

    20240612

--- .devcontainer/build_melodic.bat
+++ .devcontainer/build_melodic.bat
@@ -1,2 +1,2 @@
 rem docker pull devrt/ros-devcontainer-vscode:melodic-desktop
-docker build --build-arg BASE_IMAGE=devrt/ros-devcontainer-vscode:melodic-desktop -f Dockerfile -t gerardharkema/my-ros-devcontainer-vscode:melodic-desktop .
+docker build --build-arg BASE_IMAGE=devrt/ros-devcontainer-vscode:melodic-desktop -f Dockerfile -t my-ros-devcontainer-vscode:melodic-desktop .
